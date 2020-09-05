950cc09410c1b922067ce3cfd9ce40a72f0e6b4c
VadimBir <vasumrus@gmail.com>
2020-09-04 20:02:02 -0500

    Some changes

--- Decorator_Pattern/src/SomeNewComputer.java
+++ Decorator_Pattern/src/SomeNewComputer.java
@@ -1,10 +1,11 @@
-
+import TypeOS.*;
 
 public class SomeNewComputer extends Computer{
 
     public SomeNewComputer() 
     {
         System.out.print("\n \n New Unknown Computer");
+        typeOS = new Ubuntu();
 
     }
 }
